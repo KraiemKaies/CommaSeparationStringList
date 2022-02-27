bcae3f77244fb07d020d7b641632ae214ec272c0
kraiemkaies <kraiem.kaies@gmail.com>
2022-02-27 23:18:18 +0100

    Succefull comma separation

--- Program.cs
+++ Program.cs
@@ -7,15 +7,25 @@ class Program
     static void Main(string[] args)
     {
         List<string> listId = new();
+
+        List<int> listId1 = new();
+        List<int> listId2 = new();
+
         listId.Add("140,36");
         listId.Add("741,396");
         listId.Add("8789,96352");
         listId.Add("7485,4125");
-        List<int> TagIds = listId.SelectMany(s=>s.Split(',').Select(int.Parse)).ToList();
 
-        foreach (var item in TagIds)
+        List<int> ListIdInt = listId.SelectMany(s=>s.Split(',').Select(int.Parse)).ToList();
+
+        for (int i = 0; i < ListIdInt.Count; i++)
         {
-            Console.WriteLine(item);
+            if (i % 2 == 0) listId1.Add(ListIdInt[i]);
+            if (i % 2 == 1) listId2.Add(ListIdInt[i]);
         }
+
+        foreach (string i in listId) Console.WriteLine("Total List :" + i);
+        foreach (int i in listId1) Console.WriteLine("List 1 :" + i);
+        foreach (int i in listId2) Console.WriteLine("List 2 :" + i);
     }
 }
