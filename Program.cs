4c6cd21dacd69e257c84cab6d2fbf195c3f5bd6c
kraiemkaies <kraiem.kaies@gmail.com>
2022-02-27 22:59:28 +0100

    Add List

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        List<string> listId = new();
+        listId.Add("140,36");
+        listId.Add("741,396");
+        listId.Add("8789,96352");
+        listId.Add("7485,4125");
+        List<int> TagIds = listId.SelectMany(s=>s.Split(',').Select(int.Parse)).ToList();
+
+        foreach (var item in TagIds)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
